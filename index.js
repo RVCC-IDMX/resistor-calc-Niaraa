1ca6b7d7d7aade76bff8788a3b7094e973399794
Niara <123399425+Niaraa@users.noreply.github.com>
2023-12-03 17:56:24 -0500

    successful calculator

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
 import { getResistorOhms } from './resistor.js';
 
+const answer = document.querySelector(".answer")
 
-const color0 = document.querySelector("#color0");
-console.log(color0)
-console.log("js is working")
+const b0 = document.querySelector("#b0")
+const b1 = document.querySelector("#b1")
+const b2 = document.querySelector("#b2")
+const b3 = document.querySelector("#b3")
+
+const bands = {
+    color1: "red",
+    color2: "green",
+    multiplier: "violet",
+    tolerance: "gold"
+};
+
+const color1 = document.querySelector("#color1");
+
+color1.addEventListener("click", (e) => {
+    console.log(e.target.className.split(" "));
+    const colors = e.target.className.split(" ");
+    const color = colors[1];
+    b0.classList.remove(bands.color1)
+    b0.classList.add(color)
+    bands.color1 = color
+    const value = getResistorOhms(bands)
+    console.log(value)
+    answer.innerHTML = value
+
+});
+
+const color2 = document.querySelector("#color2");
+
+color2.addEventListener("click", (e) => {
+    console.log(e.target); // logs `false`
+    const colors = e.target.className.split(" ");
+    const color = colors[1];
+    b1.classList.remove(bands.color2)
+    b1.classList.add(color)
+    bands.color2 = color
+    const value = getResistorOhms(bands)
+    console.log(value)
+    answer.innerHTML = value
+
+});
+
+const multiplier = document.querySelector("#multiplier");
+
+multiplier.addEventListener("click", (e) => {
+    console.log(e.target); // logs `false`
+    const colors = e.target.className.split(" ");
+    const color = colors[1];
+    b2.classList.remove(bands.multiplier)
+    b2.classList.add(color)
+    bands.multiplier = color
+    const value = getResistorOhms(bands)
+    console.log(value)
+    answer.innerHTML = value
+
+});
+
+const tolerance = document.querySelector("#tolerance");
+
+tolerance.addEventListener("click", (e) => {
+    console.log(e.target); // logs `false`
+    const colors = e.target.className.split(" ");
+    const color = colors[1];
+    b3.classList.remove(bands.tolerance)
+    b3.classList.add(color)
+    bands.tolerance = color
+    const value = getResistorOhms(bands)
+    console.log(value)
+    answer.innerHTML = value
+
+});
